34d7437a22c25fb6e88881bce41e45a9e8f5f0f7
ericamatulis <erica.matulis@gmail.com>
2021-02-24 16:16:53 -0500

    Updated App, Piano to have landing page and loading functionality when user changes the instrument or song. Added silent sound so that sounds are loaded in time (pending test on different computers)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import {Piano} from './components/Piano.js'
 function App() {
   return (
     <div className="App">
+        <div id="welcome">
+            <div className="vertical-center">
+                <img src="http://courses.pianovideolessons.com/wp-content/uploads/2019/06/piano-300x242.png"/>
+                <h1>Welcome to the piano follow along game!</h1>
+                <button onClick={() => document.getElementById("welcome").hidden=true}>Begin playing!</button>
+            </div>
+        </div>
       <header className="App-header">
         <Piano />
       </header>
